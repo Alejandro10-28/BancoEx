4daa5de8529874559c80f090b98c0a23b9fe65dd
Alejandro10-28 <55776556+Alejandro10-28@users.noreply.github.com>
2020-06-01 18:29:19 -0700

    Add files via upload

---
+++ AccountSave.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoEx
+{
+    public class AccountSave:Cuenta
+    {
+        //se encapsulan los atributos que tendra la clase Accounsave
+        public DateTime Vencimiento { get; set; }
+        public decimal IngresoxMes { get; set; }
+        // Se crea metodo ingreso por mes
+        public Decimal IngresoPorMes(Cuenta IpM)
+        {
+            IngresoxMes=Convert.ToInt32(IpM.Saldo)*5;
+            
+            return IngresoxMes;
+        }
+    }
+}
---
+++ CurrentAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoEx
+{
+   public  class CurrentAccount:Cuenta
+    {
+        //Relacion de Herencia
+    
+
+            public void Reintegro(Cuenta P)
+            {
+               Console.WriteLine("Ingresa cantidad que desees retirar:");
+               decimal Retirar = Convert.ToDecimal(Console.ReadLine());
+
+            if (Retirar < P.Saldo)
+            {
+                P.Saldo -= Retirar;
+                Console.WriteLine("Tu operación ha sido procesada"+"\n"+"Saldo actual:{0}", P.Saldo);
+
+
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente");
+            }
+
+            }
+        
+    }
+}
+
